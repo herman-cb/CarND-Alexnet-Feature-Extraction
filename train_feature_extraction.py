9a0779fd3a080d9eacfa27e1742bb43c493c0c16
Herman <herman.sahota@gmail.com>
2017-02-15 03:09:39 +0000

    Add my solution training feature extraction.(WIP)

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -18,6 +18,9 @@ num_train = int(total_count * 0.8)
 X_train = X_data[0:num_train]
 y_train = y_data[0:num_train]
 
+from sklearn.preprocessing import LabelBinarizer
+
+
 X_validation = X_data[num_train:]
 y_validation = y_data[num_train:]
 
@@ -26,6 +29,7 @@ print('Number of validation examples={}'.format(total_count-num_train))
 
 # TODO: Define placeholders and resize operation.
 x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
 resized = tf.image.resize_images(x, (227, 227))
 
 # TODO: pass placeholder as first argument to `AlexNet`.
@@ -47,9 +51,52 @@ probs = tf.nn.softmax(logits)
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+y_one_hot = tf.one_hot(y, nb_classes)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, y_one_hot)
+loss_operation = tf.reduce_mean(cross_entropy)
+lr = 0.001
+
+optimizer = tf.train.AdamOptimizer()
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8W, fc8b])
+
+correct_prediction = tf.equal(tf.argmax(probs, 1), tf.argmax(y_one_hot, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+EPOCHS = 10
+BATCH_SIZE = 128
+
+from sklearn.utils import shuffle
+
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
 
 # TODO: Train and evaluate the feature extraction model.
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    num_examples = len(X_train)
+    print('Training ...')
 
-# init = tf.global_variables_initializer()
-# sess = tf.Session()
-# sess.run(init)
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x:batch_x, y:batch_y})
+        X_validation, y_validation = shuffle(X_validation, y_validation)
+        validation_acc = evaluate(X_validation, y_validation)
+        print('EPOCH = {} ...'.format(i+1))
+        print('Validation accuracy = {}'.format(validation_acc))
+        print()
+    saver.save(sess, 'model')
+    print('model saved')    
+
